f945e01517210edfcce2eb2a4ad9d5a754795a83
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2023-05-22 19:54:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,10 @@ sudo apt full-upgrade -y
 echo "2. Install base desktop environment"
 sudo apt install budgie-desktop -y
 echo "3. Install extra applications"
-sudo apt install tilix lightdm thunar pitivi git preload curl -y
+sudo apt install tilix lightdm thunar pitivi git preload -y
+sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-beta-archive-keyring.gpg https://brave-browser-apt-beta.s3.brave.com/brave-browser-beta-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list\
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg] https://brave-browser-apt-beta.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
 sudo apt update
 sudo apt install brave-browser-beta -y
 echo "4. Install cursors (Nordzy-cursors)"
